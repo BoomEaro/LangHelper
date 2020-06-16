25f7b4b6f35947ac07585f4ce70a0fac8113c21e
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2020-06-16 11:14:28 +0300

    add new versions

--- src/ru/boomearo/langhelper/objects/LangVersion.java
+++ src/ru/boomearo/langhelper/objects/LangVersion.java
@@ -3,6 +3,8 @@ package ru.boomearo.langhelper.objects;
 public enum LangVersion {
 
 	V1_12_2("1.12.2", 340),
+	V1_13_2("1.13.2", 404),
+	V1_14_4("1.14.4", 498),
 	V1_15_2("1.15.2", 578);
 	
 	private String name;
