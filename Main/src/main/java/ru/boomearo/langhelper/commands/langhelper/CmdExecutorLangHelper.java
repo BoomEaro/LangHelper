bd330771d3edb46bdde0215898aae5d8bd7587c0
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2021-06-17 14:14:48 +0300

    Improve tabcomplete in commands

--- Main/src/main/java/ru/boomearo/langhelper/commands/langhelper/CmdExecutorLangHelper.java
+++ Main/src/main/java/ru/boomearo/langhelper/commands/langhelper/CmdExecutorLangHelper.java
@@ -7,8 +7,11 @@ import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
 import ru.boomearo.langhelper.commands.AbstractExecutor;
 import ru.boomearo.langhelper.commands.CmdList;
+import ru.boomearo.langhelper.versions.LangType;
 
 public class CmdExecutorLangHelper extends AbstractExecutor {
 
@@ -25,19 +28,61 @@ public class CmdExecutorLangHelper extends AbstractExecutor {
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender cs, Command arg1, String arg2, String[] arg3) {
-        if (!cs.hasPermission("langhelper.admin")) {
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String arg2, String[] args) {
+        if (!sender.hasPermission("langhelper.admin")) {
             return empty;
         }
-        if (arg3.length == 1) {
+        if (args.length == 1) {
             List<String> matches = new ArrayList<>();
-            String search = arg3[0].toLowerCase();
+            String search = args[0].toLowerCase();
             for (String se : Arrays.asList("reload", "testitem", "testentity", "testenchant", "testenchantlevel")) {
                 if (se.toLowerCase().startsWith(search)) {
                     matches.add(se);
                 }
             }
             return matches;
+        }
+        else if (args.length == 2) {
+            if (args[0].equalsIgnoreCase("testitem") ||
+                    args[0].equalsIgnoreCase("testentity") ||
+                    args[0].equalsIgnoreCase("testenchant") ||
+                    args[0].equalsIgnoreCase("testenchantlevel")) {
+
+                List<String> matches = new ArrayList<>();
+                String search = args[1].toLowerCase();
+                for (LangType lt : LangType.values()) {
+                    if (lt.getName().toLowerCase().startsWith(search)) {
+                        matches.add(lt.getName());
+                    }
+                }
+                return matches;
+            }
+        }
+        else if (args.length == 3) {
+            if (args[0].equalsIgnoreCase("testentity")) {
+                if (!args[1].isEmpty()) {
+                    List<String> matches = new ArrayList<>();
+                    String search = args[2].toLowerCase();
+                    for (EntityType et : EntityType.values()) {
+                        if (et.name().toLowerCase().startsWith(search)) {
+                            matches.add(et.name());
+                        }
+                    }
+                    return matches;
+                }
+            }
+            else if (args[0].equalsIgnoreCase("testenchant")) {
+                if (!args[1].isEmpty()) {
+                    List<String> matches = new ArrayList<>();
+                    String search = args[2].toLowerCase();
+                    for (Enchantment ench : Enchantment.values()) {
+                        if (ench.getName().toLowerCase().startsWith(search)) {
+                            matches.add(ench.getName());
+                        }
+                    }
+                    return matches;
+                }
+            }
         }
         return empty;
     }
